94769dcebef92c4dc3b9c4df279435e1201599d4
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2026-02-06 15:28:02 -0500

    Build: Inicialización en FAZTAPI

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from datetime import datetime
+
+app = FastAPI(title="SIIT API - Tesis V1")
+
+@app.get("/api/v1/saludo")
+def saludo_tesis(usuario: str = "Invitado"):
+    return {
+        "mensaje": f"Hola {usuario}, consulta realizada con éxito",
+        "fecha": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "proyecto": "SIIT40 - Sistema de Invernadero Automatizado",
+        "estado": "Desarrollo"
+    }
