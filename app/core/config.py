8c1bb86eea2068362bda3096df7d8d328c24b77c
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2026-06-02 23:07:11 -0500

    feat: implement init_db.py with seed data

--- app/core/config.py
+++ app/core/config.py
@@ -11,6 +11,9 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    
+    SEED_ADMIN_EMAIL: str
+    SEED_ADMIN_PASSWORD: str
 
     @property
     def DATABASE_URL(self) -> str:
--- init_db.py
+++ init_db.py
@@ -1,13 +1,43 @@
+from sqlalchemy.orm import Session
 from app.core.database import engine
 from app.models.base import Base
 from app.models.user import User
-from app.models.greenhouse import Greenhouse, Specie, Plant
-from app.models.history import TelemetryHistory
+from app.models.greenhouse import Greenhouse
+from app.core.security import SecurityUtils
+from app.core.config import settings
 
 def create_tables():
-    print("Creando tablas en la base de datos...")
     Base.metadata.create_all(bind=engine)
-    print("¡Tablas creadas con éxito!")
+
+def seed_data():
+    db = Session(engine)
+    try:
+        admin_email = settings.SEED_ADMIN_EMAIL
+        existing_user = db.query(User).filter(User.email == admin_email).first()
+        
+        if not existing_user:
+            hashed_pw = SecurityUtils.hash_password(settings.SEED_ADMIN_PASSWORD)
+            new_user = User(
+                full_name="Luis Fernando Chumbes Ramos",
+                email=admin_email,
+                password=hashed_pw
+            )
+            db.add(new_user)
+            db.commit()
+            db.refresh(new_user)
+            
+            new_gh = Greenhouse(
+                user_id=new_user.id,
+                name="Invernadero Inteligente con Tecnología 4.0",
+                location="Lahuani, Challhuahuacho, Cotabambas, Apurímac",
+                latitude=-14.194344,
+                longitude=-72.353681
+            )
+            db.add(new_gh)
+            db.commit()
+    finally:
+        db.close()
 
 if __name__ == "__main__":
     create_tables()
+    seed_data()
